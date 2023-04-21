1e6fffe843f2f283255ce999d3ab9ee19489781b
Germán Ramos <gsrp0310@gmail.com>
2023-04-21 19:46:15 -0300

    feat: Product Manipulation

--- ecommerce-admin/models/Product.js
+++ ecommerce-admin/models/Product.js
@@ -1,9 +1,9 @@
-import { model, Schema } from 'mongoose'
+import { model, Schema, models } from 'mongoose'
 
-const ModelSchema = new Schema({
+const ProductSchema = new Schema({
   title: { type: String, required: true },
   description: String,
   price: { type: Number, required: true },
 })
 
-export const Product = model('product', ProductSchema)
+export const Product = models.Product || model('Product', ProductSchema)
