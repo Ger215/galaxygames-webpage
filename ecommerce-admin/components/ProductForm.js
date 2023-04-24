433e9a99c17763be9d4f204b88b3d8eeceee2ac3
Germán Ramos <gsrp0310@gmail.com>
2023-04-23 22:53:31 -0300

    feat: Upload Images

--- ecommerce-admin/components/ProductForm.js
+++ ecommerce-admin/components/ProductForm.js
@@ -7,6 +7,7 @@ export default function ProductForm({
   title: existingTitle,
   description: existingDescription,
   price: existingPrice,
+  images,
 }) {
   const [title, setTitle] = useState(existingTitle || '')
   const [description, setDescription] = useState(existingDescription || '')
@@ -28,6 +29,20 @@ export default function ProductForm({
     router.push('/products')
   }
 
+  async function uploadImages(ev) {
+    const files = ev.targe?.files
+    if (files?.length > 0) {
+      const data = new FormData()
+      for (const file of files) {
+        data.append('file', file)
+      }
+      const res = await fetch('/api/upload', {
+        method: 'POST',
+        body: data,
+      })
+    }
+  }
+
   return (
     <form onSubmit={saveProduct}>
       <label>Product Name</label>
@@ -37,6 +52,28 @@ export default function ProductForm({
         value={title}
         onChange={ev => setTitle(ev.target.value)}
       />
+      <label>Photos</label>
+      <div className="mb-2">
+        <label className="w-24 h-24 text-center cursor-pointer flex items-center justify-center text-sm gap-1 text-gray-500 rounded-lg bg-gray-200">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+            />
+          </svg>
+          <div>Upload</div>
+          <input type="file" onChange={uploadImages} className="hidden"></input>
+        </label>
+        {!images?.length && <div>No images</div>}
+      </div>
       <label>Description</label>
       <textarea
         placeholder="Description"
